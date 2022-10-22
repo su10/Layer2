f1b5c7798558794a70a3ef52508f082b92f98bd5
su10 <1687366+su10@users.noreply.github.com>
2022-10-22 11:15:22 +0900

    ILayerSettingsインターフェース追加

---
+++ Packages/com.jagapippi.layer2/Runtime/ILayerSettings.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jagapippi.Layer2
+{
+    public interface ILayerSettings
+    {
+        string LayerToName(int layer);
+        int NameToLayer(string name);
+        bool GetCollision(int layer1, int layer2);
+    }
+
+    public static class ILayerSettingsExtensions
+    {
+        public static bool GetIgnoreCollision(this ILayerSettings settings, int layer1, int layer2)
+        {
+            return (settings.GetCollision(layer1, layer2) == false);
+        }
+
+        public static string[] GetNames(this ILayerSettings settings)
+        {
+            var names = new string[Layer.MaxCount];
+
+            for (var i = 0; i < Layer.MaxCount; i++)
+            {
+                names[i] = settings.LayerToName(i);
+            }
+
+            return names;
+        }
+
+        public static void GetNamesNonAlloc(this ILayerSettings settings, IList<string> list)
+        {
+            for (var i = 0; i < Layer.MaxCount; i++)
+            {
+                list[i] = settings.LayerToName(i);
+            }
+        }
+
+        public static void ApplyCollisionMatrix(this ILayerSettings settings)
+        {
+            for (var i = 0; i < Layer.MaxCount; i++)
+            {
+                for (var j = 0; j < Layer.MaxCount - i; j++)
+                {
+                    Physics.IgnoreLayerCollision(i, j, settings.GetIgnoreCollision(i, j));
+                }
+            }
+        }
+    }
+}
