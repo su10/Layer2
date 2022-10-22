91bda6f9ba5d1b7c0c3ebc7928111569bd92ade7
su10 <1687366+su10@users.noreply.github.com>
2022-10-22 11:56:53 +0900

    LayerMaskのLayer2対応版である構造体追加

---
+++ Packages/com.jagapippi.layer2/Runtime/Layer2Mask.cs
@@ -0,0 +1,47 @@
+using System;
+using UnityEngine;
+
+namespace Jagapippi.Layer2
+{
+    [Serializable]
+    public struct Layer2Mask
+    {
+        public static string LayerToName(int layer) => Layer2Core.currentSettings.LayerToName(layer);
+        public static int NameToLayer(string layerName) => Layer2Core.currentSettings.NameToLayer(layerName);
+
+        public static int GetMask(params string[] layerNames)
+        {
+            if (layerNames == null) throw new ArgumentNullException(nameof(layerNames));
+
+            var mask = 0;
+
+            foreach (var layerName in layerNames)
+            {
+                var layerIndex = NameToLayer(layerName);
+                if (0 < layerIndex) mask |= (1 << layerIndex);
+            }
+
+            return mask;
+        }
+
+        [SerializeField] internal int _value;
+
+        public int value
+        {
+            get => _value;
+            set => _value = value;
+        }
+
+        public static implicit operator int(Layer2Mask self) => self._value;
+
+        public static implicit operator Layer2Mask(int value)
+        {
+            Layer2Mask layer2Mask;
+            layer2Mask._value = value;
+            return layer2Mask;
+        }
+
+        public static implicit operator LayerMask(Layer2Mask self) => self._value;
+        public static implicit operator Layer2Mask(LayerMask self) => (int)self;
+    }
+}
