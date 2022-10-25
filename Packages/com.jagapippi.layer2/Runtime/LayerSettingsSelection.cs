879d8ecaa7bf228a184dc8dc6b7ba5b5d6dc3be6
su10 <1687366+su10@users.noreply.github.com>
2022-10-26 00:34:38 +0900

    非再生時にLayerSettingsをApplyしたらレイヤー名をTagManager.assetに保存するようにした

--- Packages/com.jagapippi.layer2/Runtime/LayerSettingsSelection.cs
+++ Packages/com.jagapippi.layer2/Runtime/LayerSettingsSelection.cs
@@ -8,6 +8,11 @@ namespace Jagapippi.Layer2
 {
     internal static class LayerSettingsSelection
     {
+#if UNITY_EDITOR
+        private static readonly UnityEngine.Object PhysicsManager = Unsupported.GetSerializedAssetInterfaceSingleton(nameof(PhysicsManager));
+        private static readonly UnityEngine.Object Physics2DSettings = Unsupported.GetSerializedAssetInterfaceSingleton(nameof(Physics2DSettings));
+        private static readonly UnityEngine.Object TagManager = Unsupported.GetSerializedAssetInterfaceSingleton(nameof(TagManager));
+#endif
         private static ILayerSettings _active;
         public static ILayerSettings active => _active ?? EmptyLayerSettings.instance;
 
@@ -33,29 +38,53 @@ namespace Jagapippi.Layer2
 
         public static void Apply(ILayerSettings layerSettings)
         {
+            void LocalApply()
+            {
+#if UNITY_EDITOR
+                Undo.SetCurrentGroupName("Modified Properties in Project Settings");
+                Undo.RecordObjects(new[] { PhysicsManager, Physics2DSettings, TagManager }, "");
+
+                var tagManager = new SerializedObject(TagManager);
+                var layersProperty = tagManager.FindProperty("layers");
+#endif
+                for (var i = 0; i < Layer.MaxCount; i++)
+                {
+#if UNITY_EDITOR
+                    if (Application.isPlaying == false)
+                    {
+                        layersProperty.GetArrayElementAtIndex(i).stringValue = active.LayerToName(i);
+                    }
+#endif
+                    for (var j = 0; j < Layer.MaxCount - i; j++)
+                    {
+                        Physics.IgnoreLayerCollision(i, j, active.GetIgnoreCollision(i, j));
+                        Physics2D.IgnoreLayerCollision(i, j, active.GetIgnoreCollision2D(i, j));
+                    }
+                }
+#if UNITY_EDITOR
+                tagManager.ApplyModifiedProperties();
+
+                EditorUtility.SetDirty(PhysicsManager);
+                EditorUtility.SetDirty(Physics2DSettings);
+                EditorUtility.SetDirty(TagManager);
+
+                EditorApplication.ExecuteMenuItem("File/Save Project");
+#endif
+            }
+
             if (_active == layerSettings)
             {
-                active.ApplyCollisionMatrix();
+                LocalApply();
                 return;
             }
 
             var old = _active;
             _active = layerSettings;
-            active.ApplyCollisionMatrix();
+            LocalApply();
 
+#if UNITY_EDITOR
             changed?.Invoke(old, active);
-        }
-
-        private static void ApplyCollisionMatrix(this ILayerSettings settings)
-        {
-            for (var i = 0; i < Layer.MaxCount; i++)
-            {
-                for (var j = 0; j < Layer.MaxCount - i; j++)
-                {
-                    Physics.IgnoreLayerCollision(i, j, settings.GetIgnoreCollision(i, j));
-                    Physics2D.IgnoreLayerCollision(i, j, settings.GetIgnoreCollision2D(i, j));
-                }
-            }
+#endif
         }
     }
 }
